f8eb025132f9e92ea66007970581750e9ef1bee7
kaderkaya <kaderkayaarslan35@gmail.com>
2023-10-24 17:28:56 +0300

    - Changes have been made.

---
+++ app/src/main/java/com/kaderkayaarslan/kisileruygulamasi/ui/adapter/KisilerAdapter.kt
@@ -0,0 +1,54 @@
+package com.kaderkayaarslan.kisileruygulamasi.ui.adapter
+
+import android.content.Context
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.navigation.Navigation
+import androidx.recyclerview.widget.RecyclerView
+import com.google.android.material.snackbar.Snackbar
+import com.kaderkayaarslan.kisileruygulamasi.data.entity.Kişiler
+import com.kaderkayaarslan.kisileruygulamasi.databinding.CardTasarimBinding
+import com.kaderkayaarslan.kisileruygulamasi.databinding.FragmentAnasayfaBinding
+import com.kaderkayaarslan.kisileruygulamasi.ui.fragment.AnasayfaFragment
+import com.kaderkayaarslan.kisileruygulamasi.ui.fragment.AnasayfaFragmentDirections
+
+class KisilerAdapter(var mContext: Context,var kisilerListesi:List<Kişiler>)
+    : RecyclerView.Adapter<KisilerAdapter.CardTasarimTutucu>() {
+    inner class CardTasarimTutucu(tasarim:CardTasarimBinding) : RecyclerView.ViewHolder(tasarim.root){
+        var tasarim:CardTasarimBinding
+        init {
+            this.tasarim = tasarim
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CardTasarimTutucu {
+       val layoutInflater = LayoutInflater.from(mContext)
+       val tasarim = CardTasarimBinding.inflate(layoutInflater, parent, false)
+        return CardTasarimTutucu(tasarim)
+
+    }
+
+    override fun onBindViewHolder(holder: CardTasarimTutucu, position: Int) {
+        val kisi = kisilerListesi.get(position)
+       val t = holder.tasarim
+        t.textViewKisiBilgi.text = "${kisi.kisi_ad} - ${kisi.kisi_tel}"
+        t.satirCard.setOnClickListener {
+            val gecis = AnasayfaFragmentDirections.kisiDetayGecis(kisi = kisi)
+            Navigation.findNavController(it).navigate(gecis)
+        }
+
+        t.imageViewSil.setOnClickListener {
+            Snackbar.make(it,"${kisi.kisi_ad} silinsin mi?", Snackbar.LENGTH_LONG)
+                .setAction("EVET"){
+                    Log.e("Kişi Sil",kisi.kisi_id.toString())
+
+                }.show()
+
+        }
+    }
+
+    override fun getItemCount(): Int {
+     return kisilerListesi.size
+    }
+}
--- app/src/main/java/com/kaderkayaarslan/kisileruygulamasi/ui/fragment/AnasayfaFragment.kt
+++ app/src/main/java/com/kaderkayaarslan/kisileruygulamasi/ui/fragment/AnasayfaFragment.kt
@@ -1,17 +1,22 @@
 package com.kaderkayaarslan.kisileruygulamasi.ui.fragment
 
 import android.os.Bundle
+import android.util.Log
 import android.view.*
 import androidx.fragment.app.Fragment
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.SearchView
 import androidx.core.view.MenuProvider
 import androidx.lifecycle.Lifecycle
 import androidx.navigation.Navigation
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.kaderkayaarslan.kisileruygulamasi.R
+import com.kaderkayaarslan.kisileruygulamasi.data.entity.Kişiler
 import com.kaderkayaarslan.kisileruygulamasi.databinding.FragmentAnasayfaBinding
+import com.kaderkayaarslan.kisileruygulamasi.ui.adapter.KisilerAdapter
 
 
-class AnasayfaFragment : Fragment() {
+class AnasayfaFragment : Fragment(),SearchView.OnQueryTextListener {
     private lateinit var tasarim:FragmentAnasayfaBinding
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
@@ -20,12 +25,28 @@ class AnasayfaFragment : Fragment() {
         tasarim.toolbarAnasayfa.title ="Kişiler"
         (activity as AppCompatActivity).setSupportActionBar(tasarim.toolbarAnasayfa)
 
+        tasarim.rv.layoutManager = LinearLayoutManager(requireContext())
+        val kisilerListesi = ArrayList<Kişiler>()
+        val k1 = Kişiler(1,"Ahmet","1111")
+        val k2 = Kişiler(2,"Zeynep","2222")
+        val k3 = Kişiler(3,"Beyza","3333")
+        kisilerListesi.add(k1)
+        kisilerListesi.add(k2)
+        kisilerListesi.add(k3)
+
+        val adapter = KisilerAdapter(requireContext(),kisilerListesi)
+        tasarim.rv.adapter = adapter
+
         tasarim.fab.setOnClickListener {
             Navigation.findNavController(it).navigate(R.id.kisiKayıtGecis)
         }
         requireActivity().addMenuProvider(object : MenuProvider{
             override fun onCreateMenu(menu: Menu, menuInflater: MenuInflater) {
             menuInflater.inflate(R.menu.toolbar_menu,menu)
+
+                val item = menu.findItem(R.id.action_ara)
+                val searchView = item.actionView as SearchView
+                searchView.setOnQueryTextListener(this@AnasayfaFragment)
             }
 
             override fun onMenuItemSelected(menuItem: MenuItem): Boolean {
@@ -35,6 +56,26 @@ class AnasayfaFragment : Fragment() {
 
 
         return tasarim.root
+    }
+
+    override fun onQueryTextSubmit(query: String): Boolean {
+        ara(query)
+        return true
+
+    }
+
+    override fun onQueryTextChange(newText: String): Boolean {
+        ara(newText)
+        return true
+    }
+    fun ara(aramaKelimesi:String){
+        Log.e("Kişi Ara",aramaKelimesi)
+
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.e("Kişi Anasayfa","Dönüldü")
     }
 }
 
